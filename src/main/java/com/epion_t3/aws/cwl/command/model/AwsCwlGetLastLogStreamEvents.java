a113ac4d815a9bbee28cc068c1693aaf621b3ca0
Nozomu Takashima <nozomu.takashima@gmail.com>
2021-02-23 12:17:59 +0900

    mod

--- src/main/java/com/epion_t3/aws/cwl/command/model/AwsCwlGetLastLogStreamEvents.java
+++ src/main/java/com/epion_t3/aws/cwl/command/model/AwsCwlGetLastLogStreamEvents.java
@@ -1,14 +1,14 @@
 /* Copyright (c) 2017-2021 Nozomu Takashima. */
 package com.epion_t3.aws.cwl.command.model;
 
-import com.epion_t3.aws.cwl.command.runner.AwsCwlGetLogEventsRunner;
+import com.epion_t3.aws.cwl.command.runner.AwsCwlGetLogStreamEventsRunner;
 import com.epion_t3.core.common.annotation.CommandDefinition;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@CommandDefinition(id = "AwsCwlGetLastLogStreamEvents", runner = AwsCwlGetLogEventsRunner.class)
+@CommandDefinition(id = "AwsCwlGetLastLogStreamEvents", runner = AwsCwlGetLogStreamEventsRunner.class)
 public class AwsCwlGetLastLogStreamEvents extends AwsCwlBase {
     private String logGroupName;
     private String logStreamNamePrefix;
